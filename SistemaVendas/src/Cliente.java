0c2a70ba1506b73fad35f22b3393a0fc96062afa
MarkiCast <36992814+MarkiCast@users.noreply.github.com>
2020-10-13 19:15:52 -0300

    Update Cliente.java

--- SistemaVendas/src/Cliente.java
+++ SistemaVendas/src/Cliente.java
@@ -1,25 +1,4 @@
 
-public class Cliente {
-    private String name;
-    private Long cpf;
-    
-    public Cliente(String name, Long cpf) {
-        this.name = name;
-        this.cpf = cpf;
-    }
-    public Cliente(){
+public class cliente {
 
-    }
-    public String getName(){
-        return name;
-    }
-    public Long getCpf(){
-        return cpf;
-    }
-    public void setCpf(Long cpf){
-        this.cpf = cpf;
-    }
-    public void setName(String name){
-        this.name=name;
-    }
 }
